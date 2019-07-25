40c253236d0c4d07e98af9334a8dd5d18be66626
Gyuhwan Park★ <doping.cheese@gmail.com>
2019-07-25 19:09:54 +0900

    feature: 새 이스터 에그 추가

--- js/easteregg.js
+++ js/easteregg.js
@@ -3,34 +3,48 @@
     var __KEYCODE_C = 67;
     var __KEYCODE_V = 86;
 
-    window.__S = window.__C = window.__V = false;
+    var __KEYCODE_R = 82;
+    var __KEYCODE_G = 71;
+    var __KEYCODE_B = 66;
+
+    var easterEggs = [
+        { keys: [__KEYCODE_S, __KEYCODE_C, __KEYCODE_V], video: '/img/sensei.mp4' },
+        { keys: [__KEYCODE_R, __KEYCODE_G, __KEYCODE_B], video: '/img/monitor.mp4' },
+    ];
+
+    window.__pressedKeys = new Set();
 
     var __handleKey = function(event) {
-        var isKeyDown = event.type === 'keydown';
-        switch (event.keyCode) {
-            case __KEYCODE_S:
-                window.__S = isKeyDown;
-                break;
-            case __KEYCODE_C:
-                window.__C = isKeyDown;
-                break;
-            case __KEYCODE_V:
-                window.__V = isKeyDown;
-                break;
-            default:
-                break;
-        }
-
         var element = jQuery('#easter-egg');
         var videoElement = jQuery('#easter-egg > video')[0];
 
-        if ('jQuery' in window && window.__S && window.__C && window.__V) {
-            videoElement.play();
-            element.show();
-        } else {
+        if (event.type === 'keydown') {
+            window.__pressedKeys.add(event.keyCode);
+        } else if (event.type === 'keyup') {
+            if (window.__pressedKeys.has(event.keyCode)) {
+                window.__pressedKeys.delete(event.keyCode);
+            }
+
             videoElement.pause();
             element.hide();
         }
+
+
+
+        easterEggs.forEach(e => {
+            for (var k of e.keys) {
+                if (!window.__pressedKeys.has(k)) {
+                    return;
+                }
+            };
+
+            if (videoElement.paused) {
+                videoElement.src = e.video;
+                videoElement.play();
+                element.show();
+            }
+        });
+
     };
 
     window.addEventListener('keydown', __handleKey);
