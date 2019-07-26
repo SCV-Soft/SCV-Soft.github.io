029875e97222c9edd300a7da1d666185a5212eea
Gyuhwan Park★ <doping.cheese@gmail.com>
2019-07-26 13:16:34 +0900

    feature: 새 이스터에그 추가

--- js/easteregg.js
+++ js/easteregg.js
@@ -7,14 +7,25 @@
     var __KEYCODE_G = 71;
     var __KEYCODE_B = 66;
 
+    var __KEYCODE_I = 73;
+    var __KEYCODE_D = 68;
+    var __KEYCODE_O = 79;
+    var __KEYCODE_L = 76;
+
     var easterEggs = [
         { keys: [__KEYCODE_S, __KEYCODE_C, __KEYCODE_V], video: '/img/sensei.mp4' },
         { keys: [__KEYCODE_R, __KEYCODE_G, __KEYCODE_B], video: '/img/monitor.mp4' },
+        { keys: [__KEYCODE_I, __KEYCODE_D, __KEYCODE_O, __KEYCODE_L], video: '/img/idol.mp4' },
     ];
 
     window.__pressedKeys = new Set();
 
     var __handleKey = function(event) {
+        if (event.repeat) {
+            return;
+        }
+
+
         var element = jQuery('#easter-egg');
         var videoElement = jQuery('#easter-egg > video')[0];
 
